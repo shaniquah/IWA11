a7806cba89aa4ded91d7573e24580c99b749cbfc
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-05-11 07:36:57 +0200

    modified files

--- challenge-1/script.js
+++ challenge-1/script.js
@@ -1,32 +1,17 @@
-const root1 = document.querySelector('[data-key="order1"]');
-const biscuits1 = parseInt(root1.dataset.biscuits);
-const donuts1 = parseInt(root1.dataset.donuts);
-const pancakes1 = parseInt(root1.dataset.pancakes);
-const status1 = root1.querySelector('[data-delivered="false"]');
+const order1 = document.querySelector('[data-key="order1"]');
+const order2 = document.querySelector('[data-key="order2"]');
+const order3 = document.querySelector('[data-key="order3"]');
 
-const root2 = document.querySelector('[data-key="order2"]');
-const biscuits2 = parseInt(root2.dataset.biscuits);
-const donuts2 = parseInt(root2.dataset.donuts);
-const pancakes2 = parseInt(root2.dataset.pancakes);
-const status2 = root2.querySelector('[data-delivered="false"]');
+const orders = [order1, order2, order3];
 
-const root3 = document.querySelector('[data-key="order3"]');
-const biscuits3 = parseInt(root3.dataset.biscuits);
-const donuts3 = parseInt(root3.dataset.donuts);
-const pancakes3 = parseInt(root3.dataset.pancakes);
-const status3 = root3.querySelector('[data-delivered="true"]');
+orders.forEach((dl) => {
+    const biscuits = dl.querySelector('[data-biscuits]').getAttribute('data-biscuits="10"');
+    const donuts = dl.querySelector('[data-donuts]').getAttribute('data-donuts');
+    const pancakes = dl.querySelector('[data-pancakes]').getAttribute('data-pancakes');
+    const delivered = dl.querySelector('[data-delivered]').getAttribute('data-delivered');
 
-biscuits1 = root1.biscuits,
-donuts1 = root1.donuts,
-pancakes1 = root1.pancakes,
-status1 = root1.status ? Delivered : Pending
-
-biscuits2 = root2.biscuits,
-donuts2 = root2.donuts,
-pancakes2 = root2.pancakes,
-status2 = root2.status ? Delivered : Pending
-
-biscuits3 = root3.biscuits,
-donuts3 = root3.donuts,
-pancakes3 = root3.pancakes,
-status3 = root3.status ? Delivered : Pending
+    biscuits = order.querySelector('.biscuits .count').textContent;
+    donuts = order.querySelector('.donuts .count').textContent;
+    pancakes = order.querySelector('.pancakes .count').textContent;
+    delivered = order.querySelector('.status dd').textContent;
+});
